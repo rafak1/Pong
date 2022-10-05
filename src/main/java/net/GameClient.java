df0ad1585026b0037e3368c73fcf3b845a8e90e6
rafak1 <rafalkajca@gmail.com>
2022-10-05 20:23:44 +0200

    new classes

--- src/main/java/net/GameClient.java
+++ src/main/java/net/GameClient.java
@@ -1,4 +1,22 @@
 package net;
 
-public class GameClient {
+import game.Game;
+
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+
+public class GameClient implements Runnable{
+
+    private Inet4Address ipAdress;
+    private DatagramSocket socket;
+    private Game game;
+
+    public GameClient(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
